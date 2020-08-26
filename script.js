95e5b498adeed7d69a5e0d284b6ad031764149db
Gabor Balogh <grabomobil23@gmail.com>
2020-08-26 19:38:09 +0100

    extension

---
+++ script.js
@@ -0,0 +1,30 @@
+function peek(info, tab) {
+    const text = info.selectionText;
+
+    let encode = "";
+    try {
+        encode = btoa(text)
+    } catch {}
+
+    let decode = "";
+    try {
+        decode = atob(text)
+    } catch {}
+
+    const message = [
+        "𝗣𝗲𝗲𝗸𝗶𝗻𝗴 𝘃𝗮𝗹𝘂𝗲 𝗼𝗳:",
+        text,
+        "𝗧𝗲𝘅𝘁 𝘁𝗼 𝗕𝗮𝘀𝗲𝟲𝟰:",
+        decode,
+        "𝗕𝗮𝘀𝗲𝟲𝟰 𝘁𝗼 𝗧𝗲𝘅𝘁:",
+        encode
+    ].join('\n');
+
+    alert(message);
+}
+
+chrome.contextMenus.create({
+    title: "Peek Base64: %s",
+    contexts: ["selection"],
+    onclick: peek
+});
